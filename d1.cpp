ef919e44be0af1f33d2bc520c91fc484a25a775d
Atharva <athrudev@gmail.com>
2025-02-13 18:59:15 +0530

    learn other glbegin methods

--- d1.cpp
+++ d1.cpp
@@ -3,12 +3,13 @@
 
 void init()
 {
-    glClearColor(1.0,0.0,0.0,1.0);
-    glColor3i(0,1,0);
-    gluOrtho2D(0.0,500.0,0.0,500.0);
+    glClearColor(1.0,1.0,1.0,1.0);
+    glColor3i(0,0,0);
+    glPointSize(5.0);
+    gluOrtho2D(0.0,1000.0,0.0,1000.0);
 }
 
-void d(){
+void displayline(){
     glClear(GL_COLOR_BUFFER_BIT);
 
     glBegin(GL_LINES);
@@ -20,15 +21,46 @@ void d(){
     glFlush();
 }
 
+void displaypoly(){
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glBegin(GL_POLYGON);
+
+    glVertex2i(100,100);
+    glVertex2i(200,100);
+    glVertex2i(200,200);
+    glVertex2i(100,200);
+    //glVertex2i(200,200);
+
+    glEnd();
+    glFlush();
+}
+
+void displayPoints(){
+    glClear(GL_COLOR_BUFFER_BIT);       
+    glBegin(GL_POINTS)  ;
+    int c=100;
+    for(int i=0;i<=10;i++){
+
+        c += 20;
+        glVertex2i(100,c);
+
+    }
+
+    glEnd();
+    glFlush();
+    //glVertex2i(101,200);
+}
+
 int main(int c,char**v){
 
 
     glutInit(&c,v);
     glutInitDisplayMode(GLUT_RGB);
-    glutInitWindowPosition(500,500);
-    glutInitWindowSize(500,500);
+    glutInitWindowPosition(500,100);
+    glutInitWindowSize(1000,700);
     glutCreateWindow("I am noob");
     init();
-    glutDisplayFunc(d);
+    glutDisplayFunc(displayPoints);
     glutMainLoop();
 }
