fe38a15fd8825ecea1098fe210e50333a69c5967
Atharva <athrudev@gmail.com>
2025-02-21 09:05:17 +0530

    completed the olympic ring

--- BresCircle.cpp
+++ BresCircle.cpp
@@ -28,6 +28,9 @@ void plotAxis(){
     glFlush();
 }
 
+
+
+
 // Bresenham's Circle Algorithm
 void circleAlgo(int r, int h, int k) {
     int d = 3 - (2 * r);
@@ -63,16 +66,69 @@ void circleAlgo(int r, int h, int k) {
 
 void olympicRing() {
 
-    circleAlgo(100, 200, 200);
-    circleAlgo(100, 400, 200);
-    circleAlgo(100, 300, 300);
-    circleAlgo(100, 500, 300);
-    circleAlgo(100, 100, 300);
+       glColor3f(0.0, 0.129, 0.584);  // Olympic blue
+       circleAlgo(100, 100, 300);
+       
+       
+       glColor3f(1.0, 0.8, 0.0);  // Olympic yellow
+       circleAlgo(100, 300, 300);
+       
+       
+       glColor3f(0.0, 0.0, 0.0);  // Black
+       circleAlgo(100, 500, 300);
+       
+       glColor3f(0.0, 0.6, 0.2);  // Olympic green
+       circleAlgo(100, 200, 200);
+       
+       glColor3f(0.906, 0.082, 0.129);  // Olympic red
+       circleAlgo(100, 400, 200);
+}
+
+
+void drawShape() {
+    
+       
+        int x = 500;
+        int y = -400;
+        
+        // Draw outer circle
+        circleAlgo(200, x, y);
+       
+        glBegin(GL_LINES);
+
+        // First line of triangle
+        glVertex2i(500, -200);  // Top point
+        glVertex2i(327, -500);  // Bottom left point
+        
+        // Second line of triangle
+        glVertex2i(327, -500);  // Bottom left point
+        glVertex2i(673, -500);  // Bottom right point
+        
+        // Third line of triangle
+        glVertex2i(673, -500);  // Bottom right point
+        glVertex2i(500, -200);  // Back to top point
+        glEnd();
+        
+        // Draw inner circle
+        circleAlgo(80, x, y);
+    
 }
 
 void display() {
     glClear(GL_COLOR_BUFFER_BIT);
     olympicRing();
+
+    glColor3i(0, 0, 0);
+    
+    circleAlgo(100,0,0);
+
+
+    //concentric circle
+    circleAlgo(200, -300, -300); // Outer circle
+    circleAlgo(100, -300, -300); // Inner circle
+
+    drawShape();
+
     plotAxis();
     glFlush();
 }
