09cf1af74dd1762b4aa23da1990d020e70d3ffce
Atharva <athrudev@gmail.com>
2025-04-15 13:06:15 +0530

    made some correction's

--- BresCircle.cpp
+++ BresCircle.cpp
@@ -14,6 +14,75 @@ void myInit(void) {
     gluOrtho2D(-wl, wl, -wh, wh);
 }
 
+
+int sign(int val){
+    if(val<0){
+        return -1;
+    }else if(val==0){
+        return 0;
+    }else{
+        return 1;
+    }
+}
+
+void BAlgorihtm(int x1,int y1,int x2,int y2){
+
+    //validation for checking source and destination points
+    if (x1 == x2 && y1 == y2) {
+       cout << "Error: Source and Destination points are the same! Line cannot be drawn." << endl;
+       return;
+   }
+
+   int x=x1;
+   int y=y1;
+   int Dx=abs(x2-x1);
+   int Dy=abs(y2-y1);
+   int s1=sign(x2-x1);
+   int s2=sign(y2-y1);
+   int interchange;
+
+   if(Dy>Dx){
+       int temp=Dx;
+       Dx=Dy;
+       Dy=temp;
+       interchange=1;
+   }else{
+       interchange=0;
+   }
+
+   int e=2*Dy-Dx;
+
+   glBegin(GL_POINTS);
+      // glVertex2i(x,y);
+
+       for(int i=1;i<=Dx;i++){
+               glVertex2i(x, y);
+
+           while(e>=0){
+               if(interchange==1){
+                   x=x+s1;
+               }else{
+                   y=y+s2;
+               }
+               e=e-2*Dx;
+           }
+
+           if(interchange==1){
+               y=y+s2;
+           }else{
+               x=x+s1;
+           }
+           e=e+2*Dy;
+
+
+       }
+       glEnd();
+       glFlush();
+
+}
+
+
+
 //function to plot axis
 void plotAxis(){
     glBegin(GL_POINTS);
@@ -93,16 +162,11 @@ void drawShape() {
     glBegin(GL_LINES);
     
     // First line of triangle
-    glVertex2i(500, -200);  // Top point
-    glVertex2i(327, -500);  // Bottom left point
-    
+    BAlgorihtm(500, -200,327, -500); 
     // Second line of triangle
-    glVertex2i(327, -500);  // Bottom left point
-    glVertex2i(673, -500);  // Bottom right point
-    
+    BAlgorihtm(327, -500, 673, -500);     
     // Third line of triangle
-    glVertex2i(673, -500);  // Bottom right point
-    glVertex2i(500, -200);  // Back to top point
+    BAlgorihtm(673, -500, 500, -200); 
     glEnd();
     
     // Draw inner circle
